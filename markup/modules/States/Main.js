8d34efc810d87c01bbbd7f1e2a093432e231addf
grnch <grnch.net@gmail.com>
2016-10-25 21:01:12 +0300

    Create wheels module, beginning.

--- markup/modules/States/Main.js
+++ markup/modules/States/Main.js
@@ -1,3 +1,7 @@
+import { model } from '../../modules/Model/Model';
+import { config } from '../../modules/Util/Config';
+import { Wheels } from '../../modules/Wheels/Wheels';
+
 export class Main {
     constructor(game) {
 
@@ -14,6 +18,25 @@ export class Main {
 
     }
     create() {
-        
+        // const centerEl = this.add.sprite(this.world.centerX, this.world.centerY, '3', '3-n.png').anchor.set(0.5);
+
+        const machineContainer = this.add.group(this.mainContainer, 'gameMachine');
+        machineContainer.position.set(this.world.centerX, this.world.centerY);
+
+        let wheels = [];
+        let elSize = config[model.state('res')].elements;
+        for (let i = -2; i < 3; i++) {
+            wheels.push(new Wheels({
+                state: this,
+                parent: machineContainer,
+                position: {
+                    x: i * elSize.width,
+                    y: 0
+                },
+                elSize
+            }));
+        }
+
+        console.log(wheels);
     }
 }
