d29c200cf9f01ad320de01f85961b809edbb83a9
Nastya <nastya.volkovinskaya@gmail.com>
2016-10-26 15:14:16 +0300

    BG added

--- markup/modules/States/Main.js
+++ markup/modules/States/Main.js
@@ -17,18 +17,24 @@ export class Main {
 
     }
     create() {
-        let gameMachine = this.add.sprite(0, 0, 'gameMachine', null, this.mainContainer);
+        this.drawMainBG();
+        this.drawMainContainer();
         buttons.drawMobileButtons(this.buttonsContainer, this, this.mainContainer.width);
 
         model.data('mainXLeft', 2 * model.data('buttonsDelta'));
         model.data('mainXRight', this.game.width - this.mainContainer.width - model.data('buttonsDelta') * 2);
-        // gameMachine.x = model.data('buttonsDelta') * 2;
         this.mainContainer.x = model.data('mainXLeft');
+        console.log('this.mainContainer.x', this.mainContainer.x);
     }
+
     drawMainBG() {
+        let mainBG = this.add.sprite(0, 0, 'mainBG', null, this.bgContainer);
 
     }
+
     drawMainContainer() {
+        let gameBG = this.add.sprite(0, 0, 'gameBG', null, this.mainContainer);
+        let gameMachine = this.add.sprite(0, 0, 'gameMachine', null, this.mainContainer);
 
     }
 }
