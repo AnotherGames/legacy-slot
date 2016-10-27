2ee46f0bca72b0b6da743dacdb0eb3cc6c0bff98
Gaboa <dmitry.zhabko@gmail.com>
2016-10-27 16:51:19 +0300

    export model in browser console

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -3,6 +3,7 @@ import { Preload } from '../../modules/States/Preload';
 import { Init } from '../../modules/States/Init';
 import { Main } from '../../modules/States/Main';
 import { FS } from '../../modules/States/FS';
+import { model } from 'modules/Model/Model';
 
 let game = new Phaser.Game(1280, 720, Phaser.AUTO, 'game');
 
@@ -11,3 +12,5 @@ game.state.add('Preload', Preload);
 game.state.add('Init', Init);
 game.state.add('Main', Main);
 game.state.add('FS', FS);
+
+export { model };
