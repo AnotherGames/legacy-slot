0bbdbda98bb085d3e0e994d7ee2b273197170d4d
Gaboa <dmitry.zhabko@gmail.com>
2016-10-27 16:51:57 +0300

    add machine config for HD and fullHD

--- markup/modules/Util/Config.js
+++ markup/modules/Util/Config.js
@@ -10,12 +10,20 @@ export let config = {
         elements: {
             width: 256,
             height: 240
+        },
+        machine: {
+            x: 60,
+            y: 113
         }
     },
     HD: {
         elements: {
             width: 192,
             height: 180
+        },
+        machine: {
+            x: 45,
+            y: 85
         }
     }
 };
