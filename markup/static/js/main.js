87a70695c7c76723953a1d98bd2ee62d6761e905
Nastya <nastya.volkovinskaya@gmail.com>
2016-10-27 19:16:24 +0300

    menu started

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -3,8 +3,10 @@ import { Preload } from '../../modules/States/Preload';
 import { Init } from '../../modules/States/Init';
 import { Main } from '../../modules/States/Main';
 import { FS } from '../../modules/States/FS';
+import { model } from '../../modules/Model/Model';
 
 let game = new Phaser.Game(1280, 720, Phaser.AUTO, 'game');
+model.el('game', game);
 
 game.state.add('Boot', Boot, true);
 game.state.add('Preload', Preload);
