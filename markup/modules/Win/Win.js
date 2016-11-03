8b5eaf5eb346a7a275ff83c53053e5d6c083a37e
Gaboa <dmitry.zhabko@gmail.com>
2016-11-03 15:07:11 +0200

    add win splash on numbers

--- markup/modules/Win/Win.js
+++ markup/modules/Win/Win.js
@@ -1,30 +1,66 @@
 import { model } from 'modules/Model/Model';
 import { events } from 'modules/Events/Events';
+import { config } from 'modules/Util/Config';
 
 export let win = (function() {
 
-    function drawWin() {
+    function showWin() {
         let data = model.data('rollResponse');
         let winTotalData = data.Balance.TotalWinCoins;
+        let winLines = data.WinLines;
+
         if (winTotalData === 0) return;
 
         let game = model.el('game');
         let mainContainer = model.el('mainContainer');
-        let gameMachine = mainContainer.getAt(2);
 
         let winTop = game.add.group();
         mainContainer.addAt(winTop, 3);
         model.el('winTop', winTop);
+
+        drawTotalWin(winTotalData);
+
+        winLines.forEach((winLine) => {
+            drawWinNumber(winLine.Line);
+        })
+
+    }
+
+    function drawTotalWin(winTotalData) {
+
+        let game = model.el('game');
+        let mainContainer = model.el('mainContainer');
+        let winTop = model.el('winTop');
+        let gameMachine = mainContainer.getAt(2);
 
         let winTotal = game.add.sprite(gameMachine.width / 2, gameMachine.height / 2, 'winTotal', null, winTop);
         winTotal.anchor.set(0.5);
 
         let winTotalText = game.add.text(gameMachine.width / 2, gameMachine.height / 2 + 5, winTotalData, {font: '60px Helvetice, Arial', fill: '#e8b075', align: 'center'}, winTop);
         winTotalText.anchor.set(0.5);
+
     }
 
-    function drawWinNumbers() {
+    function drawWinNumber(number) {
+        if (number < 0) return;
+        let game = model.el('game');
+        let winTop = model.el('winTop');
 
+        let winSplash = game.add.sprite(0, 0, 'win', null, winTop);
+        winSplash.anchor.set(0.5);
+        winSplash.position.x = config[model.state('res')].win[number][0].x;
+        winSplash.position.y = config[model.state('res')].win[number][0].y;
+        winSplash.animations.add('win', Phaser.Animation.generateFrameNames('Splash-Splash', 1, 14, '.png', 1), 15, false);
+        winSplash.animations.play('win');
+        winSplash.animations.getAnimation('win').killOnComplete = true;
+
+        let winSplash2 = game.add.sprite(0, 0, 'win', null, winTop);
+        winSplash2.anchor.set(0.5);
+        winSplash2.position.x = config[model.state('res')].win[number][1].x;
+        winSplash2.position.y = config[model.state('res')].win[number][1].y;
+        winSplash2.animations.add('win', Phaser.Animation.generateFrameNames('Splash-Splash', 1, 14, '.png', 1), 15, false);
+        winSplash2.animations.play('win');
+        winSplash2.animations.getAnimation('win').killOnComplete = true;
     }
 
     function cleanWin() {
@@ -34,7 +70,7 @@ export let win = (function() {
         }
     }
 
-    events.on('roll:end', drawWin);
+    events.on('roll:end', showWin);
     events.on('roll:start', cleanWin);
 
     return {
