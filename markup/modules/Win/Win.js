17e3d90be078fed1898f6a7d1d727c752753e744
Gaboa <dmitry.zhabko@gmail.com>
2016-11-03 13:49:27 +0200

    init Win module

--- markup/modules/Win/Win.js
+++ markup/modules/Win/Win.js
@@ -3,7 +3,15 @@ import { events } from 'modules/Events/Events';
 
 export let win = (function() {
 
+    function drawWin() {
+        let game = model.el('game');
+        let data = model.data('rollResponse');
 
+        let winTop = game.add.group();
+        game.add.sprite(0, 0, 'winTotal');
+    }
+
+    events.on('roll:end', drawWin);
 
     return {
 
