14bf991c600fe44a6afe1c6a489060255b166503
grnch <grnch.net@gmail.com>
2016-11-03 14:01:18 +0200

    Update Glista module.

--- markup/modules/Glista/Glista.js
+++ markup/modules/Glista/Glista.js
@@ -41,6 +41,7 @@ export class Glista {
         // инитим внутрение параметры
         this.timer = 0;
         this.progress = 0;
+        this.isRemove = false;
 
         this.pointStartPos = {
             x: this.elSize.width * -5,
@@ -52,9 +53,14 @@ export class Glista {
             y: 0
         };
 
-        this.sprite = this.game.add.sprite( this.pointStartPos.x, this.pointStartPos.y, 'glista', null, param.parent);
-        this.sprite.anchor.set(0.90, 0.5);
-        this.sprite.visible = false;
+        this.sprites = [];
+        for (let atlasInd = 0; atlasInd < 6; atlasInd++) {
+            let sprite = this.game.add.sprite( atlasInd * -80, 0, 'glistaAtlas', 5 - atlasInd, param.parent);
+            sprite.anchor.set(0.8, 0.5);
+            sprite.visible = false;
+            this.sprites.push(sprite);
+        }
+
         this.pointPos = [];
 
         for (let columInd = 0; columInd < 5; columInd++) {
@@ -72,6 +78,10 @@ export class Glista {
         *callback: Function
     */
     start(path, time = config.glista.time, callback) {
+        if (this.isRemove) {
+            console.warn('start: Glista is remove.');
+            return;
+        }
         if (!Array.isArray(path)) {
             console.error('start: path is not Array.', path);
             return;
@@ -93,28 +103,49 @@ export class Glista {
             console.error('start: time is not Number', time);
             return;
         }
-        if (time <= 0) {
+        if (time === 0) {
             console.error('start: time is incorrectly.', time);
             return;
         }
 
-        this.bezierPath = {
-            x: [this.pointStartPos.x],
-            y: [this.pointStartPos.y]
-        };
+        this.direction = (time < 0) ? -1 : 1;
+        time = Math.abs(time);
 
-        for (let pathInd = 0; pathInd < path.length; pathInd++) {
-            if (typeof (path[pathInd]) !== 'number') continue;
-            this.bezierPath.x.push(this.pointPos[pathInd][ path[pathInd] ].x);
-            this.bezierPath.y.push(this.pointPos[pathInd][ path[pathInd] ].y);
+
+        if (this.direction < 0) {
+            this.bezierPath = {
+                x: [this.pointEndPos.x],
+                y: [this.pointEndPos.y]
+            };
+            for (let pathInd = path.length - 1; pathInd >= 0; pathInd--) {
+                if (typeof (path[pathInd]) !== 'number') continue;
+                this.bezierPath.x.push(this.pointPos[pathInd][ path[pathInd] ].x);
+                this.bezierPath.y.push(this.pointPos[pathInd][ path[pathInd] ].y);
+            }
+            this.bezierPath.x.push(this.pointStartPos.x);
+            this.bezierPath.y.push(this.pointStartPos.y);
+        } else {
+            this.bezierPath = {
+                x: [this.pointStartPos.x],
+                y: [this.pointStartPos.y]
+            };
+            for (let pathInd = 0; pathInd < path.length; pathInd++) {
+                if (typeof (path[pathInd]) !== 'number') continue;
+                this.bezierPath.x.push(this.pointPos[pathInd][ path[pathInd] ].x);
+                this.bezierPath.y.push(this.pointPos[pathInd][ path[pathInd] ].y);
+            }
+            this.bezierPath.x.push(this.pointEndPos.x);
+            this.bezierPath.y.push(this.pointEndPos.y);
         }
 
-        this.bezierPath.x.push(this.pointEndPos.x);
-        this.bezierPath.y.push(this.pointEndPos.y);
+        let nodes = [];
 
-        this.sprite.visible = true;
-        this.sprite.position.set(this.bezierPath.x[0], this.bezierPath.y[0]);
-        this.sprite.rotation = 0;
+        for (let spriteInd = 0; spriteInd < 6; spriteInd++) {
+            this.sprites[spriteInd].visible = true;
+            this.sprites[spriteInd].position.set(this.bezierPath.x[0], this.bezierPath.y[0]);
+            this.sprites[spriteInd].rotation = 0;
+            nodes.push({ x: this.bezierPath.x[0], y: this.bezierPath.y[0], angle: 0 });
+        }
 
         let _this = this;
         this._clock = this.game.time.create(true);
@@ -122,8 +153,8 @@ export class Glista {
         this._clock.start();
         this.timer = 0;
         this.progress = 0;
+        let margin = 0.03;
 
-        let node = { x: this.bezierPath.x[0], y: this.bezierPath.y[0], angle: 0 };
 
         let anim = function () {
             _this.timer = time - _this._clock.duration;
@@ -132,13 +163,31 @@ export class Glista {
                 _this.progress = 1;
             }
 
-            let px = _this.game.math.catmullRomInterpolation(_this.bezierPath.x, _this.progress);
-            let py = _this.game.math.catmullRomInterpolation(_this.bezierPath.y, _this.progress);
-            let angle = _this.game.math.angleBetweenPoints(node, { x: px, y: py, angle: 0 });
-            node = { x: px, y: py, angle: angle };
+            if (_this.progress < 0.5) {
+                margin = 0.03 - 0.015 * _this.progress;
+            } else {
+                margin = 0.03 - 0.015 * (1 - _this.progress);
+            }
 
-            _this.sprite.position.set(px, py);
-            _this.sprite.rotation = angle;
+            for (let spriteInd = 0; spriteInd < 6; spriteInd++) {
+                let progress = _this.progress - margin * spriteInd;
+                if (progress <= 0) {
+                    break;
+                }
+
+                let px = _this.game.math.catmullRomInterpolation(_this.bezierPath.x, progress);
+                let py = _this.game.math.catmullRomInterpolation(_this.bezierPath.y, progress);
+                let angle = _this.game.math.angleBetweenPoints(nodes[spriteInd], { x: px, y: py, angle: 0 });
+                nodes[spriteInd] = { x: px, y: py, angle: angle };
+
+                _this.sprites[spriteInd].position.set(px, py);
+                if (progress < 0.5) {
+                    _this.sprites[spriteInd].scale.set(1.2 - 1.2 * progress);
+                } else {
+                    _this.sprites[spriteInd].scale.set(1.2 * progress);
+                }
+                _this.sprites[spriteInd].rotation = angle;
+            }
 
             if (_this.progress === 1) {
                 _this.game.frameAnims.splice(_this.game.frameAnims.indexOf(anim), 1);
@@ -151,6 +200,13 @@ export class Glista {
         this.game.frameAnims.push(anim);
     }
     remove() {
-        this.sprite.destroy();
+        if (this.isRemove) {
+            console.warn('start: Glista is remove.');
+            return;
+        }
+        this.isRemove = true;
+        this.sprites.forEach((sprite) => {
+            sprite.destroy();
+        });
     }
 }
