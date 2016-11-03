2c8d98aed443b6aa6ee0ecf6880efe28837984fb
Gaboa <dmitry.zhabko@gmail.com>
2016-11-03 13:48:34 +0200

    add load win tables

--- markup/modules/States/Preload.js
+++ markup/modules/States/Preload.js
@@ -90,6 +90,8 @@ function loadMainAssets(game) {
     game.load.image('gameShadow', 'game/gameShadow.png');
     game.load.image('infoRules', 'other/infoRules.png');
     game.load.image('popup', 'other/popup.png');
+    game.load.image('winLine', 'win/winLineRect.png');
+    game.load.image('winTotal', 'win/winTotalRect.png');
     game.load.atlasJSONArray('menuButtons', 'menu/menu.png', 'menu/menu.json');
     game.load.atlasJSONArray('footerButtons', 'footer/footerButtons.png', 'footer/footerButtons.json');
     if (model.flag('desktop')) {
