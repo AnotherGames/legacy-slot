6eafa073111a2f28ae1301a88109cae7363baee6
grnch <grnch.net@gmail.com>
2016-11-03 13:56:46 +0200

    Update preload Glista Assets (atlas).

--- markup/modules/States/Preload.js
+++ markup/modules/States/Preload.js
@@ -127,4 +127,5 @@ function loadFSAssets(game) {
 
 function loadGlistaAssets(game) {
     game.load.image('glista', 'glista/glista.png');
+    game.load.atlasJSONArray('glistaAtlas', 'glista/glista.png', 'glista/glista.json');
 }
