c53b6dfbab65e37c12ff114145df110b09eb59ca
grnch <grnch.net@gmail.com>
2016-11-08 15:24:43 +0200

    Update FSCharacter module.

--- markup/modules/FSCharapter/FSCharapter.js
+++ markup/modules/FSCharapter/FSCharapter.js
@@ -27,19 +27,48 @@ export class FSCharapter {
             console.error('constructor: param.position.y is undefined', param);
             return;
         }
-
+        // инит входящие параметры
         this.game = param.game;
         this.position = param.position;
+        // инит внутрение параметры
+        this.level = 0;
+        console.log(this.level);
 
-        let animBG = this.game.add.spine(
+        this.char = this.game.add.spine(
             this.position.x,        // X positon
             this.position.y,        // Y position
             'FSCharapter'     // the key of the object in cache
         );
-        animBG.setAnimationByName(
-            0,          // Track index
-            '1',     // Animation's name
-            true        // If the animation should loop or not
-        );
+        this.char.setAnimationByName(0, 'idle' + this.level, true);
+    }
+    Up() {
+        if (this.level >= 5) return;
+        this.level = this.level + 1;
+        console.log(this.level);
+        if (this.level < 5) {
+            this.char.setAnimationByName(0, 'idle' + this.level, true);
+        } else {
+            this.char.setAnimationByName(0, 'idle5', false);
+            this.char.setAnimationByName(0, 'idle6', false);
+            this.char.addAnimationByName(0, 'Win0', false);
+            this.char.addAnimationByName(0, 'Win1', true);
+
+            let switcher = true;
+            let _this = this;
+            let randomAnim = function () {
+                _this.game.time.events.add(5000, () => {
+                    switcher = !switcher;
+                    if (switcher) {
+                        _this.char.setAnimationByName(0, 'Win2', false);
+                    } else {
+                        _this.char.setAnimationByName(0, 'Win3', false);
+                    }
+                    _this.char.addAnimationByName(0, 'Win1', true);
+                    randomAnim();
+                }, _this);
+            };
+            randomAnim();
+        }
+        this.char.setToSetupPose();
     }
 }
