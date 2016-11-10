ffa650e8d8af81f0dba8e0ea46ae0734b774b951
Gaboa <dmitry.zhabko@gmail.com>
2016-11-10 19:10:13 +0200

    refactor buttonsView

--- markup/modules/Buttons/ButtonsView.js
+++ markup/modules/Buttons/ButtonsView.js
@@ -2,71 +2,71 @@ import { model } from 'modules/Model/Model';
 
 export let view = (() => {
 
-    function drawSpinButton({
-            x = 0,
-            y = model.el('game').world.centerY,
-            container = model.group('buttons')
-    }) {
-        const game = model.el('game');
-        const spinButton = game.add.sprite(x, y, 'mobileButtons', 'spin.png', container);
-        spinButton.anchor.set(0.5);
-        model.el('spinButton', spinButton);
-        return spinButton;
-    }
+    let draw = {
 
-    function drawAutoButton({
-            x = 0,
-            y = 0,
-            container = model.group('buttons')
-    }) {
-        const game = model.el('game');
-        const autoButton = game.add.sprite(x, y, 'mobileButtons', 'auto.png', container);
-        autoButton.anchor.set(0.5);
-        model.el('autoButton', autoButton);
-        return autoButton;
-    }
+        SpinButton: function({
+                x = 0,
+                y = model.el('game').world.centerY,
+                container = model.group('buttons')
+        }) {
+            const game = model.el('game');
+            const spinButton = game.add.sprite(x, y, 'mobileButtons', 'spin.png', container);
+            spinButton.anchor.set(0.5);
+            model.el('spinButton', spinButton);
+            return spinButton;
+        },
 
-    function drawBetButton({
-            x = 0,
-            y = 0,
-            container = model.group('buttons')
-    }) {
-        const game = model.el('game');
-        const betButton = game.add.sprite(x, y, 'mobileButtons', 'setBet.png', container);
-        betButton.anchor.set(0.5);
-        model.el('betButton', betButton);
-        return betButton;
-    }
+        AutoButton: function({
+                x = 0,
+                y = 0,
+                container = model.group('buttons')
+        }) {
+            const game = model.el('game');
+            const autoButton = game.add.sprite(x, y, 'mobileButtons', 'auto.png', container);
+            autoButton.anchor.set(0.5);
+            model.el('autoButton', autoButton);
+            return autoButton;
+        },
 
-    function drawMenuButton({
-            x = 0,
-            y = 0,
-            container = model.group('buttons')
-    }) {
-        const game = model.el('game');
-        const menuButton = game.add.sprite(x, y, 'mobileButtons', 'menu.png', container);
-        menuButton.anchor.set(0.5);
-        model.el('menuButton', menuButton);
-        return menuButton;
-    }
+        BetButton: function({
+                x = 0,
+                y = 0,
+                container = model.group('buttons')
+        }) {
+            const game = model.el('game');
+            const betButton = game.add.sprite(x, y, 'mobileButtons', 'setBet.png', container);
+            betButton.anchor.set(0.5);
+            model.el('betButton', betButton);
+            return betButton;
+        },
 
-    function drawSoundButton({
-            x = 0,
-            y = 0,
-            container = model.group('buttons')
-    }) {
-        const game = model.el('game');
-        const soundButton = game.add.sprite(x, y, 'mobileButtons', 'sound.png', container);
-        soundButton.anchor.set(0.5);
-        model.el('soundButton', soundButton);
-        return soundButton;
-    }
+        MenuButton: function({
+                x = 0,
+                y = 0,
+                container = model.group('buttons')
+        }) {
+            const game = model.el('game');
+            const menuButton = game.add.sprite(x, y, 'mobileButtons', 'menu.png', container);
+            menuButton.anchor.set(0.5);
+            model.el('menuButton', menuButton);
+            return menuButton;
+        },
+
+        SoundButton: function({
+                x = 0,
+                y = 0,
+                container = model.group('buttons')
+        }) {
+            const game = model.el('game');
+            const soundButton = game.add.sprite(x, y, 'mobileButtons', 'sound.png', container);
+            soundButton.anchor.set(0.5);
+            model.el('soundButton', soundButton);
+            return soundButton;
+        }
+
+    };
 
     return {
-        drawSpinButton,
-        drawBetButton,
-        drawAutoButton,
-        drawMenuButton,
-        drawSoundButton
-    }
+        draw
+    };
 })();
