1f5e5b369231921dbd3ed3439f3e2690c21d31d3
Gaboa <dmitry.zhabko@gmail.com>
2016-11-10 17:05:46 +0200

    new handle style

--- markup/modules/Footer/FooterController.js
+++ markup/modules/Footer/FooterController.js
@@ -11,15 +11,15 @@ export let controller = (() => {
         view.drawTime({});
 
         let homeButton = view.drawHomeButton({});
-            homeButton.onInputDown.add(handleHome);
+            homeButton.onInputDown.add(handle.Home);
         let menuButton = view.drawMenuButton({});
-            menuButton.onInputDown.add(handleMenu);
+            menuButton.onInputDown.add(handle.Menu);
         let soundButton = view.drawSoundButton({});
             soundButton.freezeFrames = true;
-            soundButton.onInputDown.add(handleSound);
+            soundButton.onInputDown.add(handle.Sound);
         let fastButton = view.drawFastButton({});
             fastButton.freezeFrames = true;
-            fastButton.onInputDown.add(handleFast);
+            fastButton.onInputDown.add(handle.Fast);
     }
 
     function initMobile() {
@@ -27,65 +27,63 @@ export let controller = (() => {
         view.drawTime({});
 
         let homeButton = view.drawHomeButton({});
-            homeButton.onInputDown.add(handleHome);
+            homeButton.onInputDown.add(handle.Home);
     }
 
     function updateTime() {
         view.updateTime();
     }
 
-    function handleHome() {
-        sound.sounds.button.play();
-        request.send('Logout')
-            .then((response) => {
-                console.log('Logout response:', response);
+    const handle = {
+        Menu: function() {
+            sound.sounds.button.play();
+
+            $('#volume').prop('value', sound.volume * 100);
+            $('#checkSound').prop('checked', model.state('sound'));
+            $('#checkMusic').prop('checked', model.state('music'));
+            $('#fastSpin').prop('checked', model.state('fastRoll'));
+            $('#isAnimations').prop('checked', model.state('isAnimations'));
+
+            $('#settings').removeClass('closed');
+            $('#darkness').removeClass('closed');
+
+            $('#darkness').on('click', function () {
+                $('#settings').addClass('closed');
+                $('#darkness').addClass('closed');
+                $('.history').addClass('closed');
+                $('#darkness').off();
             });
-        window.history.back();
-    }
-
-    function handleMenu() {
-        sound.sounds.button.play();
-
-        $('#volume').prop('value', sound.volume * 100);
-        $('#checkSound').prop('checked', model.state('sound'));
-        $('#checkMusic').prop('checked', model.state('music'));
-        $('#fastSpin').prop('checked', model.state('fastRoll'));
-        $('#isAnimations').prop('checked', model.state('isAnimations'));
-
-        $('#settings').removeClass('closed');
-        $('#darkness').removeClass('closed');
-
-        $('#darkness').on('click', function () {
-            $('#settings').addClass('closed');
-            $('#darkness').addClass('closed');
-            $('.history').addClass('closed');
-            $('#darkness').off();
-        });
-
-    }
-
-    function handleSound() {
-        let soundButton = model.el('soundButton');
-        if (sound.volume > 0) {
-            soundButton.frameName = 'soundOff.png';
-            sound.volume = 0;
-        } else {
-            soundButton.frameName = 'soundOn.png';
-            sound.volume = 100;
+        },
+        Sound: function() {
+            let soundButton = model.el('soundButton');
+            if (sound.volume > 0) {
+                soundButton.frameName = 'soundOff.png';
+                sound.volume = 0;
+            } else {
+                soundButton.frameName = 'soundOn.png';
+                sound.volume = 100;
+            }
+        },
+        Fast: function () {
+            sound.sounds.button.play();
+            let fastButton = model.el('fastButton');
+            if (model.state('fastRoll')) {
+                model.state('fastRoll', false);
+                fastButton.frameName = 'fastSpinOff.png';
+            } else {
+                model.state('fastRoll', true);
+                fastButton.frameName = 'fastSpinOn.png';
+            }
+        },
+        Home: function() {
+            sound.sounds.button.play();
+            request.send('Logout')
+                .then((response) => {
+                    console.log('Logout response:', response);
+                });
+            window.history.back();
         }
-    }
-
-    function handleFast() {
-        sound.sounds.button.play();
-        let fastButton = model.el('fastButton');
-        if (model.state('fastRoll')) {
-            model.state('fastRoll', false);
-            fastButton.frameName = 'fastSpinOff.png';
-        } else {
-            model.state('fastRoll', true);
-            fastButton.frameName = 'fastSpinOn.png';
-        }
-    }
+    };
 
     return {
         initDesktop,
