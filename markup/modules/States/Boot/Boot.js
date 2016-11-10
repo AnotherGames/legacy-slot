9a5aff522eb07e5196e7508713b8f55c29b143c2
Gaboa <dmitry.zhabko@gmail.com>
2016-11-10 11:33:52 +0200

    Boot State update, add BootController

--- markup/modules/States/Boot/Boot.js
+++ markup/modules/States/Boot/Boot.js
@@ -1,57 +1,15 @@
-import { model } from 'modules/Model/Model';
 import { events } from 'modules/Util/Events';
-import { request } from 'modules/Util/Request';
+import { bootController } from 'modules/States/Boot/BootController';
 
 export class Boot {
     constructor(game) {
 
     }
     init() {
-        console.info('Boot State!');
-        request.send('Initialise', 'fsBonus')
-            .then(this.parseInitData)
-            .catch((error) => {
-                console.error(error.message);
-            });
-
-        if (this.game.device.desktop) {
-            this.game.scale.setGameSize(1920, 1080);
-            model.state('desktop', true);
-            model.state('res', 'fullHD');
-        } else {
-            if (this.game.device.iOS || this.game.device.iPhone) {
-                $('body, html').addClass('ios');
-            }
-            this.game.scale.setGameSize(1280, 720);
-            model.state('mobile', true);
-            model.state('res', 'HD');
-        }
-        this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        console.log('Boot State!');
+        events.trigger('boot:init');
     }
     preload() {
-        this.load.path = `static/img/content/${model.state('res')}/`;
-        this.load.image('preloadBar', 'preloader/preloaderBar.png');
-        this.load.atlasJSONHash('preloadCoin', 'preloader/coin-0.png', 'preloader/coin.json');
-    }
-    create() {
-        this.state.start('Preload');
-    }
-
-    parseInitData(data) {
-        model.data('sessionID', data.SessionID);
-        model.data('initBalance', data.Balance);
-        model.data('lines', data.Lines);
-        model.data('symbols', data.Symbols);
-        model.data('saved', data.Saved);
-        model.data('firstScreen', data.FirstScreen);
-        let currentBalance = {
-            betValue: data.Balance.BetLevel[0],
-            coinsValue: data.Balance.CoinValue[0] / 100,
-            currency: data.Balance.Currency,
-            scoreCents: data.Balance.ScoreCents,
-            scoreCoins: data.Balance.ScoreCoins
-        };
-        model.data('currentBalance', currentBalance);
-        model.state('balance:init', true);
+        events.trigger('boot:preload');
     }
 }
---
+++ markup/modules/States/Boot/BootController.js
@@ -0,0 +1,55 @@
+import { model } from 'modules/Model/Model';
+import { events } from 'modules/Util/Events';
+import { request } from 'modules/Util/Request';
+
+export let controller = (() => {
+
+    function init() {
+        request.send('Initialise', 'normal')
+            .then((initData) => {
+                model.initStates(initData);
+                model.initSettings(initData.Settings);
+                model.initBalance(initData.Balance);
+                model.initSaved(initData.Saved);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+
+        _checkDevice();
+    }
+
+    function preload() {
+        const game = model.el('game');
+
+        loadPreloadAssets();
+
+        game.load.onLoadComplete.add(() => {
+            game.state.start('Preload');
+        });
+    }
+
+    function loadPreloadAssets() {
+        const game = model.el('game');
+
+        game.load.path = `static/img/content/${model.state('res')}/preloader/`;
+        game.load.image('preloadBar', 'preloaderBar.png');
+        game.load.atlasJSONHash('preloadCoin', 'coin-0.png', 'coin.json');
+    }
+
+    function _checkDevice() {
+        const game = model.el('game');
+
+        if (game.device.desktop) {
+            game.scale.setGameSize(1920, 1080);
+            model.state('res', 'fullHD');
+        } else {
+            game.scale.setGameSize(1280, 720);
+            model.state('res', 'HD');
+        }
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    }
+
+    events.on('boot:init', init);
+    events.on('boot:preload', preload);
+})();
