11c1db79603b03a9738a2f1afaa36ecd2a1088d5
Gaboa <dmitry.zhabko@gmail.com>
2016-11-10 18:50:44 +0200

    Mobile Settings update

--- markup/modules/MobileSettings/Controller.js
+++ markup/modules/MobileSettings/Controller.js
@@ -4,90 +4,51 @@ import { sound } from 'modules/Sound/Sound';
 import { controller as globalController } from 'modules/Controller/Controller';
 
 export let controller = (() => {
-    let game;
-    let overlay;
-    let container;
 
-    let _clickEvent = {
-        BackButton: function () {
+    let game;
+
+    let handle = {
+        close: function () {
+            model.state('settings', 'close');
+
             sound.sounds.button.play();
-            globalController.mobile.settings.close();
+
+            view.hide.Settings({});
+            view.hide.Overlay({})
+                .onComplete.add(() => {
+                    model.el('settingsOverlay').visible = false;
+                });
         },
-        Overlay: function () {
-            globalController.mobile.settings.close();
+        open: function () {
+            model.state('settings', 'open');
+            view.show.Settings({});
+            view.show.Overlay({});
         }
     };
 
     function init() {
         game = model.el('game');
 
-        overlay = view.drawOverlay({});
-        container = view.drawContainer({});
-        view.drawBG({});
-        view.drawBorder({});
-        view.drawTitle({});
-        const backButton = view.drawBackButton({});
+        let overlay = view.draw.Overlay({});
+            overlay.inputEnabled = true;
+            overlay.input.priorityID = 10;
+            overlay.events.onInputDown.add(handle.close);
 
-        backButton.inputEnabled = true;
-        backButton.input.priorityID = 12;
-        backButton.events.onInputDown.add(_clickEvent.BackButton);
+        view.draw.Container({});
+        view.draw.BG({});
+        view.draw.Border({});
+        view.draw.Title({});
 
-        overlay.inputEnabled = true;
-        overlay.input.priorityID = 10;
-        overlay.events.onInputDown.add(_clickEvent.Overlay);
-
-        if (model.state('side') === 'left') {
-            container.x = game.world.width;
-        } else {
-            container.x = -container.x;
-        }
+        let backButton = view.draw.BackButton({});
+            backButton.inputEnabled = true;
+            backButton.input.priorityID = 12;
+            backButton.events.onInputDown.add(handle.close);
 
         model.state('settings', 'close');
     }
 
-    function open({
-        time = 700,
-        callback = function () {}
-    }) {
-        if (model.state('side') === 'left') {
-            container.x = game.world.width;
-            game.add.tween(container).to( { x: game.world.width - container.width }, time, 'Quart.easeOut', true);
-        } else {
-            container.x = -container.width;
-            game.add.tween(container).to( { x: 0 }, time, 'Quart.easeOut', true);
-        }
-
-        overlay.alpha = 0;
-        overlay.visible = true;
-        console.log(overlay);
-        game.add.tween(overlay).to( { alpha: 0.5 }, time, 'Quart.easeOut', true)
-        .onComplete.add(() => {
-            model.state('settings', 'open');
-            callback();
-        });
-    }
-
-    function close({
-        time = 700,
-        callback = function () {}
-    }) {
-        if (model.state('side') === 'left') {
-            game.add.tween(container).to( { x: game.world.width }, time, 'Quart.easeOut', true);
-        } else {
-            game.add.tween(container).to( { x: -container.width }, time, 'Quart.easeOut', true);
-        }
-
-        game.add.tween(overlay).to( { alpha: 0 }, time, 'Quart.easeOut', true)
-        .onComplete.add(() => {
-            model.state('settings', 'close');
-            overlay.visible = false;
-            callback();
-        });
-    }
-
     return {
         init,
-        open,
-        close
+        handle
     };
 })();
