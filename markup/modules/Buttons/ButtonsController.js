8ed31b0cf39f270a55f45229a56ae33d547d7ef9
Nastya <nastya.volkovinskaya@gmail.com>
2016-11-11 13:23:35 +0200

    mobile buttons add handlers

--- markup/modules/Buttons/ButtonsController.js
+++ markup/modules/Buttons/ButtonsController.js
@@ -9,11 +9,25 @@ export let controller = (() => {
     function init() {
         game = model.el('game');
 
-        view.draw.SpinButton({});
-        view.draw.AutoButton({});
-        view.draw.BetButton({});
-        view.draw.MenuButton({});
-        view.draw.SoundButton({});
+        let spinButton = view.draw.SpinButton({});
+            spinButton.inputEnabled = true;
+            spinButton.events.onInputDown.add(handle.spinButton);
+
+        let autoButton = view.draw.AutoButton({});
+            autoButton.inputEnabled = true;
+            autoButton.events.onInputDown.add(handle.autoButton);
+
+        let betButton = view.draw.BetButton({});
+            betButton.inputEnabled = true;
+            betButton.events.onInputDown.add(handle.betButton);
+
+        let menuButton = view.draw.MenuButton({});
+            menuButton.inputEnabled = true;
+            menuButton.events.onInputDown.add(handle.menuButton);
+
+        let soundButton = view.draw.SoundButton({});
+            soundButton.inputEnabled = true;
+            soundButton.events.onInputDown.add(handle.soundButton);
 
         setButtonsX();
         setButtonsY();
@@ -64,6 +78,54 @@ export let controller = (() => {
         menuButton.y= autoButton.y - autoButton.width / 2 - buttonsDelta - menuButton.width / 2;
         soundButton.y = betButton.y + betButton.width / 2 + buttonsDelta + soundButton.width / 2;
 
+    }
+
+    const handle = {
+        spinButton: function () {
+            let spinButton = model.el('spinButton');
+            if (spinButton.frameName === 'spinEmpty.png') return;
+            sound.sounds.button.play();
+            events.trigger('roll:request');
+        },
+
+        autoButton: function() {
+            sound.sounds.button.play();
+            let autoButton = model.el('autoButton');
+            if (model.state('menu') === 'opened') return;
+            if (autoButton.frameName === 'stop.png') {
+                events.trigger('autoplay:stop');
+            } else {
+                events.trigger('menu:showMenu', 'auto');
+            }
+        },
+
+        betButton: function() {
+            let betButton = model.el('betButton');
+            if (betButton.frameName === 'setBetOut.png') return;
+            sound.sounds.button.play();
+            if (model.state('menu') === 'opened') return;
+            events.trigger('menu:showMenu', 'bet');
+        },
+
+        menuButton: function() {
+            if (controller.isEvent) return;
+            if (model.state('menu') === 'open') return;
+
+            sound.sounds.button.play();
+            // controller.mobile.settings.open();
+        },
+
+        soundButton: function() {
+            let soundButton = model.el('soundButton');
+            if (sound.volume > 0) {
+                soundButton.frameName = 'soundOut.png';
+                sound.volume = 0;
+            } else {
+                soundButton.frameName = 'sound.png';
+                sound.volume = 1;
+                sound.sounds.button.play();
+            }
+        }
     }
 
     return {
