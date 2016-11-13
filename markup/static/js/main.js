e7a8537d2d50fa0942c5061fbc7b73729794605a
grnch <grnch.net@gmail.com>
2016-11-13 14:54:28 +0200

    init Keyboard module in main.js

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,6 +1,9 @@
 import { model } from 'modules/Model/Model';
 import { events } from 'modules/Util/Events';
 
+import { keyboard } from 'modules/Keyboard/Keyboard';
+keyboard.init();
+
 const game = new Phaser.Game(1280, 720, Phaser.AUTO, 'game');
 model.el('game', game);
 
