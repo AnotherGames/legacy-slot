01d936871fd8f7cb27a5d8b7d6363ff23739398c
Gaboa <dmitry.zhabko@gmail.com>
2016-11-15 13:41:05 +0200

    turn off no connect

--- markup/modules/States/Init/Controller.js
+++ markup/modules/States/Init/Controller.js
@@ -6,7 +6,7 @@ export class Init {
 
     }
     init() {
-        model.state('isNoConnect', true);
+        model.state('isNoConnect', false);
         model.state('side', 'left');
         model.state('autoPanel', false);
         model.state('fastRoll', false);
