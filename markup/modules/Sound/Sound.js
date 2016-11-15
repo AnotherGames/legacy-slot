2a86bf0c1a7a0d1e1e20f6570d073f60049fd514
grnch <grnch.net@gmail.com>
2016-11-15 14:13:46 +0200

    Fix Sound module.

--- markup/modules/Sound/Sound.js
+++ markup/modules/Sound/Sound.js
@@ -60,6 +60,8 @@ export let sound = {
         sound = true,
         music = true
     }) {
+        // model.data('soundModule', sound);
+
         if (this.game) {
             let keys = Object.keys(this.sounds);
             keys.forEach((key) => {
@@ -75,14 +77,22 @@ export let sound = {
         this.game = game;
         // Fon
         this.music.fon = game.add.audio('fon', 1, true);
+        this.music.fon.mute = !music;
         this.music.fsFon = game.add.audio('fsFon', 1, true);
+        this.music.fsFon.mute = !music;
         this.music.startPerehod = game.add.audio('startPerehod', 1, true);
+        this.music.startPerehod.mute = !music;
         this.music.finishPerehod = game.add.audio('finishPerehod', 1, true);
+        this.music.finishPerehod.mute = !music;
         // Sound
         this.sounds.button = game.add.audio('buttonClick');
+        this.sounds.button.mute = !sound;
         this.sounds.baraban = game.add.audio('baraban');
+        this.sounds.baraban.mute = !sound;
         this.sounds.lineWin = game.add.audio('lineWin');
+        this.sounds.lineWin.mute = !sound;
         this.sounds.lineWin2 = game.add.audio('lineWin2');
+        this.sounds.lineWin2.mute = !sound;
 
         model.state('volume', volume);
         model.state('sound', sound);
