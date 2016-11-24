d1870876668883dc70368375f5ace6b43c6a7ab0
grnch <grnch.net@gmail.com>
2016-11-24 19:25:02 +0200

    Add sound switcher on init state.

--- markup/modules/States/Init/Controller.js
+++ markup/modules/States/Init/Controller.js
@@ -35,7 +35,52 @@ export class Init {
                 view.playYoyoTween({});
             });
 
+        let soundContainer = game.add.group();
+        soundContainer.position.set(game.width - 500, game.height - 100);
+        let style = { font: "bold 42px Arial", fill: "#f3eba0"};
+        let textSound = game.add.text(0, 0, "Sound:", style, soundContainer);
+        this.styleOff = { font: "bold 42px Arial", fill: "#474747"};
+        this.textOff = game.add.text(170, 0, "Off", style, soundContainer);
+        this.textOff.setStyle(this.styleOff);
+        this.styleOn = { font: "bold 42px Arial", fill: "#b8ff31"};
+        this.textOn = game.add.text(350, 0, "On", style, soundContainer);
+        this.textOn.setStyle(this.styleOn);
+
+        let graphics = game.add.graphics(0, 0);
+        graphics.beginFill(0x6da600, 1);
+        graphics.drawRoundedRect(0, 0, 70, 30, 150);
+
+        let sprite = game.add.sprite(290, 25, graphics.generateTexture(), null, soundContainer);
+        sprite.anchor.set(0.5);
+        sprite.inputEnabled = true;
+        sprite.events.onInputDown.add(this.switchSound, this);
+        graphics.destroy();
+
+        let graphics2 = game.add.graphics(0, 0);
+        graphics2.beginFill(0xffffff, 1);
+        graphics2.drawCircle(0, 0, 30);
+        this.sprite2 = game.add.sprite(310, 25, graphics2.generateTexture(), null, soundContainer);
+        this.sprite2.anchor.set(0.5);
+        graphics2.destroy();
+
         view.firstDarkness();
+    }
+
+    switchSound() {
+        let game = model.el('game');
+        if (game.sound.volume > 0) {
+            game.sound.volume = 0;
+            this.sprite2.x = 270;
+            model.state('initScreen', false);
+            this.textOff.setStyle(this.styleOn);
+            this.textOn.setStyle(this.styleOff);
+        } else {
+            game.sound.volume = 1;
+            this.sprite2.x = 310;
+            model.state('initScreen', true);
+            this.textOff.setStyle(this.styleOff);
+            this.textOn.setStyle(this.styleOn);
+        }
     }
 
     handlePlay() {
