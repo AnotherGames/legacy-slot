c2770dd31eefb049b434dfd1bce04214481ba062
Gaboa <dmitry.zhabko@gmail.com>
2016-11-24 12:20:00 +0200

    add url params fix #128 fix #130

--- markup/modules/Util/Request.js
+++ markup/modules/Util/Request.js
@@ -20,9 +20,19 @@ export let request = (() => {
 
     function send(name, options) {
         let url;
+        let params = getAllUrlParams();
+        console.log('Params form URL: ', params);
         switch (name) {
             case 'Initialise':
-                url = `${serviceUrl}/_${name}/${userID}/${casinoID}/${mode[options]}`;
+                // Авторизация
+                if (params.demo === 'true') {
+                    name = `${name}Demo`;
+                }
+                if (params.session) {
+                    url = `${serviceUrl}/_${name}/${params.session}/${mode[options]}`; // Вставил SessionID;
+                } else {
+                    url = `${serviceUrl}/_${name}/${userID}/${casinoID}/${mode[options]}`;
+                }
                 break;
             case 'Roll':
                 url = `${serviceUrl}/_${name}/${model.data('sessionID')}/${model.balance('betValue')}/${model.balance('coinValue') * 100}`;
@@ -56,7 +66,71 @@ export let request = (() => {
             }
         });
     }
+
+    function getAllUrlParams(url) {
+
+        // get query string from url (optional) or window
+        let queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+        // we'll store the parameters here
+        let obj = {};
+
+        // if query string exists
+        if (queryString) {
+
+            // stuff after # is not part of query string, so get rid of it
+            queryString = queryString.split('#')[0];
+
+            // split our query string into its component parts
+            let arr = queryString.split('&');
+
+            for (let i = 0; i < arr.length; i++) {
+                // separate the keys and the values
+                let a = arr[i].split('=');
+
+                // in case params look like: list[]=thing1&list[]=thing2
+                let paramNum = undefined;
+                let paramName = a[0].replace(/\[\d*\]/, function(v) {
+                    paramNum = v.slice(1, -1);
+                    return '';
+                });
+
+                // set parameter value (use 'true' if empty)
+                let paramValue = typeof(a[1]) === 'undefined' ? true : a[1];
+
+                // (optional) keep case consistent
+                paramName = paramName.toLowerCase();
+                paramValue = paramValue.toLowerCase();
+
+                // if parameter name already exists
+                if (obj[paramName]) {
+                    // convert value to array (if still string)
+                    if (typeof obj[paramName] === 'string') {
+                        obj[paramName] = [obj[paramName]];
+                    }
+                    // if no array index number specified...
+                    if (typeof paramNum === 'undefined') {
+                        // put the value on the end of the array
+                        obj[paramName].push(paramValue);
+                    }
+                    // if array index number specified...
+                    else {
+                        // put the value at that index number
+                        obj[paramName][paramNum] = paramValue;
+                    }
+                }
+                // if param name doesn't exist yet, set it
+                else {
+                    obj[paramName] = paramValue;
+                }
+            }
+        }
+
+        return obj;
+    }
+
     return {
-        send
+        send,
+        getAllUrlParams
     };
 })();
