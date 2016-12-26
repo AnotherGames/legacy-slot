f18f854fda305dc99f84db9c98506952abc8dd92
Outfire <knikolaik@gmail.com>
2016-12-26 12:56:20 +0200

    fix transition sound

--- markup/modules/States/FS/FSController.js
+++ markup/modules/States/FS/FSController.js
@@ -111,6 +111,8 @@ export class FS {
 
         // Играем фоновую музыку
         soundController.music.stopMusic('startPerehod');
+        soundController.music.stopMusic('fon');
+        soundController.music.stopMusic('initFon');
         soundController.music.playMusic('fsFon');
 
         // Отрисовуем основной контейнер
