3fb47a1ab196c36b90e9343a75d7abdae5f6c054
Nykolaj <knikolaik@gmail.com>
2017-01-19 15:36:43 +0200

    fix fullscreen

--- markup/modules/States/Init/InitController.js
+++ markup/modules/States/Init/InitController.js
@@ -76,8 +76,6 @@ export class Init {
     handlePlay() {
         const game = model.el('game');
 
-        // if (model.mobile) game.scale.startFullScreen();
-        // else this.fullScreen();
         game.scale.startFullScreen();
 
         document.body.addEventListener('touchstart', () => {
@@ -90,14 +88,6 @@ export class Init {
             game.state.start('Main');
         })
         game.camera.fade(0x000000, 500)
-    }
-
-    fullScreen(element) {
-        let _e = element || document.querySelector('#game');
-
-        if (_e.requestFullScreen) _e.requestFullScreen();
-        else if (_e.mozRequestFullScreen) _e.mozRequestFullScreen();
-        else if (_e.webkitRequestFullScreen) _e.webkitRequestFullScreen();
     }
 
     checkSettingsWidth() {
