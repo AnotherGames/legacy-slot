e0c7df97d149759509590475c30f92537ba257a9
Outfire <knikolaik@gmail.com>
2017-01-20 18:30:38 +0200

    fix autoplay balance check

--- markup/modules/Autoplay/AutoplayController.js
+++ markup/modules/Autoplay/AutoplayController.js
@@ -1,4 +1,5 @@
 import { model } from 'modules/Model/Model';
+import { view as mainView } from 'modules/States/Main/MainView';
 
 import { controller as rollController } from 'modules/Roll/RollController';
 import { controller as panelController } from 'modules/Panel/PanelController';
@@ -7,7 +8,11 @@ import { controller as buttonsController } from 'modules/Buttons/ButtonsControll
 export let controller = (() => {
 
     function start(count) {
-
+        if (!model.checkBalance()) {
+            mainView.draw.showPopup({message: 'You have low balance on your account', balance: true});
+            stop();
+            return;
+        }
         // Ищменяем состояния автоплея
         model.state('autoplay:start', true);
         model.state('autoplay:end', false);
@@ -27,6 +32,11 @@ export let controller = (() => {
     }
 
     function next() {
+      if (!model.checkBalance()) {
+          mainView.draw.showPopup({message: 'You have low balance on your account', balance: true});
+          stop();
+          return;
+      }
 
         // Проверка тонких настроек автоплея
         checkSettings();
