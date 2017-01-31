a92764c9dc05ba5bf1308bb5f951f0a9251f6698
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-01-31 14:49:48 +0200

    Fix font-size of balance on desktop, add shadows

--- markup/modules/Balance/BalanceView.js
+++ markup/modules/Balance/BalanceView.js
@@ -97,18 +97,25 @@ export let view = (() => {
             betSumValue = model.balance('betSum'),
             coinValueAmount = model.balance('coinValue'),
             betValueAmount = model.balance('betValue'),
-            sumStyle = {font: 'bold 27px Titania, Arial', fill: '#fff', align: 'center'},
-            valueStyle = {font: 'bold 29px Titania, Arial', fill: '#fff', align: 'center'},
-            y = [-125, -35],
-            x = [240, 455, 1067, 1315]
+            sumStyle = {font: 'bold 32px Titania, Arial', fill: '#fff', align: 'center'},
+            valueStyle = {font: 'bold 37px Titania, Arial', fill: '#fff', align: 'center'},
+            sumCoinStyle = {font: 'bold 28px Titania, Arial', fill: '#fff', align: 'center'},
+            valueCoinStyle = {font: 'bold 26px Titania, Arial', fill: '#fff', align: 'center'},
+            y = [-123, -37],
+            x = [240, 455, 1065, 1315]
         }) {
 
-            let coinSum = game.add.text(x[3], y[0], `${coinSumValue.toFixed(0)}`, sumStyle, container);
+            let coinSum = game.add.text(x[3], y[0], `${coinSumValue.toFixed(0)}`, sumCoinStyle, container);
             let betSum = game.add.text(x[0], y[0], `${betSumValue.toFixed(0)}`, sumStyle, container);
 
-            let coinValue = game.add.text(x[2], y[1], `${coinValueAmount}`, valueStyle, container);
+            let coinValue = game.add.text(x[2], y[1], `${coinValueAmount}`, valueCoinStyle, container);
             let betValue = game.add.text(x[1], y[1], `${betValueAmount}`, valueStyle, container);
 
+            coinSum.setShadow(5, 5, 'rgba(0, 0, 0, 0.7)', 8);
+            betSum.setShadow(5, 5, 'rgba(0, 0, 0, 0.7)', 8);
+            coinValue.setShadow(5, 5, 'rgba(0, 0, 0, 0.7)', 8);
+            betValue.setShadow(5, 5, 'rgba(0, 0, 0, 0.7)', 8);
+68
             model.el('coinSum', coinSum);
             model.el('betSum', betSum);
             model.el('coinValue', coinValue);
