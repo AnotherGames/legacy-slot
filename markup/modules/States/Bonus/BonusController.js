d910cece9b3190b559aec1aeed6fbc0c32070727
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-02-01 12:12:10 +0200

    Init Bonus state

---
+++ markup/modules/States/Bonus/BonusController.js
@@ -0,0 +1,17 @@
+import { model } from 'modules/Model/Model';
+
+export class Bonus {
+
+    init() {
+        console.log('I am inited!');
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
+
+}
