2f81bdaa978ec3ab46eb98508380f0b76e557ce1
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-02-01 17:15:38 +0200

    Disable settings width change

--- markup/modules/States/Init/InitController.js
+++ markup/modules/States/Init/InitController.js
@@ -45,8 +45,6 @@ export class Init {
     create() {
         let game = model.el('game')
 
-        this.checkSettingsWidth();
-
         view.drawBG();
         view.drawLogo();
 
@@ -101,12 +99,6 @@ export class Init {
             game.state.start('Main');
         })
         game.camera.fade(0x000000, 500)
-    }
-
-    checkSettingsWidth() {
-        let settingsWidth = $('#settings').width();
-        let multiplay = window.innerWidth / settingsWidth * 0.8;
-        $('#settings').css('transform', 'translate(-50%, -50%) scale(' + multiplay + ',' + multiplay + ')');
     }
 
     drawSoundTrigger() {
