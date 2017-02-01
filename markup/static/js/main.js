ead65ac466e3e1923b9e82a7c3d7078a1c7b59ac
Dmitry Zhabko <dmitry.zhabko@gmail.com>
2017-02-01 12:11:10 +0200

    Add Bonus state to main.js

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -8,11 +8,13 @@ import { Preload } from 'modules/States/Preload/PreloadController';
 import { Init } from 'modules/States/Init/InitController';
 import { Main } from 'modules/States/Main/MainController';
 import { FS } from 'modules/States/FS/FSController';
+import { Bonus } from 'modules/States/Bonus/BonusController';
 
 game.state.add('Boot', Boot, true);
 game.state.add('Preload', Preload);
 game.state.add('Init', Init);
 game.state.add('Main', Main);
 game.state.add('FS', FS);
+game.state.add('Bonus', Bonus);
 
 export { model };
