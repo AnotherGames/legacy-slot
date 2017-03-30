07db7c029022291798d9f6f064198a797c20b5a4
Nykolaj <knikolaik@gmail.com>
2017-03-30 16:08:47 +0300

    Add request submodule

--- markup/modules/States/Boot/BootController.js
+++ markup/modules/States/Boot/BootController.js
@@ -1,11 +1,16 @@
 import { model } from 'modules/Model/Model';
-import { request } from 'modules/Util/Request';
+import { request } from '../../../../Info/Request';
 
 export class Boot {
 
     init() {
         model.state('isNoConnect', false);
         const game = model.el('game');
+        request.setMode({
+            normal: 'calipso2',
+            fsBonus: 'califs1 ',
+            doorsBonus: 'califind1'
+        });
 
         request.send('Initialise', 'normal')
             .then((initData) => {
--- markup/modules/States/FS/FSController.js
+++ markup/modules/States/FS/FSController.js
@@ -1,6 +1,6 @@
 import { model } from 'modules/Model/Model';
 import { config } from 'modules/Util/Config';
-import { request } from 'modules/Util/Request';
+import { request } from '../../../../Info/Request';
 
 import { view as fsView } from 'modules/States/FS/FSView';
 import { view as transitionView } from 'modules/Transition/TransitionView';
