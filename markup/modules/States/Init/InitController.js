ddb150741d506d5bec6f1f93e0b033fc661dcd46
Nykolaj <knikolaik@gmail.com>
2017-07-03 12:02:31 +0300

    Fix multitouch

--- markup/modules/States/Init/InitController.js
+++ markup/modules/States/Init/InitController.js
@@ -22,6 +22,7 @@ export class Init {
     create() {
         let game = model.el('game');
 
+	    game.input.maxPointers = 1;
 	    this.game.plugins.add(new Phaser.Plugin.SaveCPU(this));
 
         game.camera.flash(0x000000, 500);
