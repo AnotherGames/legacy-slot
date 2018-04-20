0e46b7c64cfcd86af94daf5eeb1fc380782a3616
Nykolaj <knikolaik@gmail.com>
2018-04-20 11:27:03 +0300

    Add fullscreen state

--- markup/modules/Model/Model.js
+++ markup/modules/Model/Model.js
@@ -154,6 +154,7 @@ export let model = (() => {
 		model.state('doorFinish', false);
 		model.data('lastClickedDoor', 0);
 		model.state('fontNotLoaded', true);
+		model.state('fullscreen', false);
 	}
 
 	function initSettings() {
